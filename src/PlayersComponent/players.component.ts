11660fa4773d2bd3d67d5b475c36889f0c0f3816
AndreaValeroSanz <avalerosanz@uoc.edu>
2024-11-12 18:49:25 +0100

    commit conexión con bdd funcional

--- src/PlayersComponent/players.component.ts
+++ src/PlayersComponent/players.component.ts
@@ -1,4 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
 import { FullscreenModalComponent } from '../MediaComponent/fullscreen-modal/fullscreen-modal.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -11,22 +14,19 @@ import { DetailComponent } from '../DetailComponent/detail.component';
   selector: 'app-players',
   styleUrls: ['./players.component.css'],
   standalone: true, // Indica que este componente puede ser utilizado de manera independiente
-  imports: [DetailComponent, CommonModule, FullscreenModalComponent, NavBarComponent, AppPlayersComponentPipes, HeroComponent], // Importa otros componentes y módulos necesarios
+  imports: [DetailComponent, CommonModule, FullscreenModalComponent, NavBarComponent, AppPlayersComponentPipes, HeroComponent, AsyncPipe], // Importa otros componentes y módulos necesarios
   templateUrl: './players.component.html' // Define la plantilla HTML del componente
 })
 export class PlayersComponent {
+  
+  firestore: Firestore = inject(Firestore);
+  players: Observable<any[]>;
 
-  players = [
-    { name: 'Jayson Tatum', ppg: '27.0', rpg: '7.1', apg: '5.7', age: 25, height:'2.03 m' ,weight:'95 Kg', image: 'Players/playerCard1.webp' },
-    { name: 'Kemba Walker', ppg: '25.0', rpg: '4.1', apg: '5.9', age: 29, height:'1.83 m' ,weight:'84 Kg', image: 'Players/walker.webp' },
-    { name: 'Stephen Curry', ppg: '26.0', rpg: '5.9', apg: '6.5', age: 34,height:'1.88 m' ,weight:'84 Kg', image: 'Players/curry.webp' },
-    { name: 'Lebron James', ppg: '26.0', rpg: '6.8', apg: '6.0', age: 38, height:'2.06 m',weight:'113 Kg', image: 'Players/lebron.webp' },
-    { name: 'Giannis Antetokounmpo', ppg: '24.0', rpg: '4.6', apg: '5.9', age: 28, height:'2.11 m' ,weight:'110 Kg', image: 'Players/giannis.webp' },
-    { name: 'Russell Westbrook', ppg: '25.0', rpg: '5.9', apg: '6.0', age: 35, height:'1.93 m' ,weight:'91 Kg', image: 'Players/westbrook.webp' },
-    { name: 'Nikola Jokic', ppg: '23.0', rpg: '4.1', apg: '5.0', age: 27, height:'2.11 m',weight:'129 Kg', image: 'Players/jokic.webp' },
-    { name: 'Dejounte Murray', ppg: '23.0', rpg: '3.0', apg: '5.0', age: 26, height:'1.96 m',weight:'82 Kg', image: 'Players/murray.webp' },
-    { name: 'Joel Embiid', ppg: '22.0', rpg: '3.5', apg: '4.5', age: 30, height:'2.13 m' ,weight:'127 Kg', image: 'Players/embiid.webp' }
-  ];
+  constructor() {
+    const aCollection = collection(this.firestore, 'jugadores')
+    this.players = collectionData(aCollection);
+  }
+
 
 
   isCardEnlarged = false; // Estado para controlar si la tarjeta está agrandada
